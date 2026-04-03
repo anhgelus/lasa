c5486ecc9f46bcef6dbd60f7e8345169a3a848ce
Anhgelus Morhtuuzh <william@herges.fr>
2026-04-03 16:32:16 +0200

    feat(cli): load config

--- cmd/lasad/run.go
+++ cmd/lasad/run.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"log/slog"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"tangled.org/anhgelus.world/lasa/cmd/internal"
+	"tangled.org/anhgelus.world/lasa/cmd/lasad/config"
 )
 
 func handleRunHelp() {
@@ -33,20 +36,41 @@ func handleRun(args []string) {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 	defer cancel()
 
+	slog.Info("loading config...", "path", configPath)
+	cfg, err := config.Load(configPath)
+	if err != nil {
+		panic(err)
+	}
+	ctx = context.WithValue(ctx, keyCfg, cfg)
+
 	mux := http.NewServeMux()
 
 	ch := make(chan error, 1)
 
 	go func() {
-		ch <- http.ListenAndServe(":8000", mux)
+		slog.Info("starting...")
+		ch <- http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), middlewares(mux, ctx))
 	}()
-	var err error
 	select {
 	case <-ctx.Done():
-		err = ctx.Err()
+		err = context.Cause(ctx)
 	case err = <-ch:
 	}
+	slog.Info("exiting")
 	if err != nil {
 		panic(err)
 	}
+}
+
+func middlewares(h http.Handler, ctx context.Context) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		slog.Debug("request", "path", r.URL.Path)
+		defer func() {
+			if err := recover(); err != nil {
+				slog.Error("panic!", "err", err)
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+		}()
+		h.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
